58535f994c36ca96832414b6c076ffc02074c376
Daiki Ueno <ueno@unixuser.org>
2010-12-06 17:01:27 +0900

    Use gnome-autogen.sh to generate configure and Makefiles.
    BUG=none
    TEST=manual
    
    Review URL: http://codereview.appspot.com/3475041

--- autogen.sh
+++ autogen.sh
@@ -1,13 +1,27 @@
 #!/bin/sh
-set -e
-set -x
+# Run this to generate all the initial makefiles, etc.
 
-autopoint
-libtoolize --automake --copy
-aclocal -I m4
-autoheader
-automake --add-missing --copy
-autoconf
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
+PKG_NAME="ibus-m17n"
+
+(test -f $srcdir/configure.ac \
+  && test -f $srcdir/README ) || {
+    echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
+    echo " top-level $PKG_NAME directory"
+    exit 1
+}
+
+which gnome-autogen.sh || {
+    echo "You need to install gnome-common from the GNOME CVS"
+    exit 1
+}
+
 export CFLAGS="-g -O0"
 export CXXFLAGS="$CFLAGS"
-./configure --enable-maintainer-mode $*
+
+ACLOCAL_FLAGS="$ACLOCAL_FLAGS -I m4"
+REQUIRED_AUTOMAKE_VERSION=1.10
+
+. gnome-autogen.sh
